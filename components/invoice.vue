2aa42c8b056f50405f826f784dc992efdd95a492
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-31 08:02:55 +0530

    styles invoice list components

--- components/invoice.vue
+++ components/invoice.vue
@@ -30,5 +30,45 @@ const props = defineProps({
 </script>
 
 <style lang="scss" scoped>
+    .invoice {
+        text-decoration: none;
+        cursor: pointer;
+        gap: 1rem;
+        margin-bottom: 1rem;
+        color: #fff;
+        border-radius: 20px;
+        padding: 28px 32px;
+        background-color: #1e2139;
+        align-items: center;
 
+        span {
+            font-size: 14px;
+        }
+
+        .left {
+           align-items: center;
+           flex-basis: 60%;
+           gap: 1rem;
+
+           span {
+                flex: 1;
+           }
+
+            .track-number {
+              text-transform: uppercase;
+            }
+        }
+
+        .right {
+            gap: 1rem;
+            flex-basis: 40%;
+            align-items: center;
+
+            .price {
+                flex: 1;
+                font-size: 1rem;
+                font-weight: 600;
+            }
+        }
+    }
 </style>
