038bf6fc7832108a3ab766b66b728e9afe0d92f8
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-31 23:08:38 +0530

    implements state managment of detail invoice page

--- pages/InvoiceView/[invoiceId].vue
+++ pages/InvoiceView/[invoiceId].vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        hii
+    <div class="">
+        {{ currentInvoice[0].invoiceId }}
     </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+    name: 'InvoiceView'
+    let invoices = ref([]);
+    let currentInvoice = ref(null);
+
+    const route = useRoute();
+
+    // Initialize the store
+    const store = useGlobalStore();
+
+    //accessing states
+    invoices = computed(() => store.invoiceData);
+
+    const getCurrentInvoice = () => {        
+        store.SET_CURRENT_INVOICE(invoices.value, route.params.invoiceId);       
+    }
+
+    getCurrentInvoice();
+
+    currentInvoice = computed(() => store.currentInvoiceArray);
+    console.log(currentInvoice.value);
 </script>
 
 <style lang="scss" scoped>
+div{
+    color: white;
+}
 </style>
