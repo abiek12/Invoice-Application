ec8dbebf2f32f4024d0e04ea4b2c5087809c1d54
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-01 22:54:22 +0530

    completes basic styling of invoice view

--- pages/InvoiceView/[invoiceId].vue
+++ pages/InvoiceView/[invoiceId].vue
@@ -26,14 +26,14 @@
         <!-- Invoice Details -->
          <div class="invoice-details flex flex-column">
             <div class="top flex">
-                <div class="left flex">
+                <div class="left flex flex-column">
                     <p><span>#</span>{{ currentInvoice[0].invoiceId }}</p>
                     <p>{{ currentInvoice[0].productDescription }}</p>
                 </div>
                 <div class="right flex flex-column">
                     <p>{{ currentInvoice[0].billerStreetAddress }}</p>
                     <p>{{ currentInvoice[0].billerCity }}</p>
-                    <p>{{ currentInvoice[0].billerZipcode }}</p>
+                    <p>{{ currentInvoice[0].billerZipCode }}</p>
                     <p>{{ currentInvoice[0].billerCountry }}</p>
                 </div>
             </div>
@@ -49,7 +49,7 @@
                     <p>{{ currentInvoice[0].clientName }}</p>
                     <p>{{ currentInvoice[0].clientStreetAddress }}</p>
                     <p>{{ currentInvoice[0].clientCity }}</p>
-                    <p>{{ currentInvoice[0].clientZipcode }}</p>
+                    <p>{{ currentInvoice[0].clientZipCode }}</p>
                     <p>{{ currentInvoice[0].clientCountry }}</p>
                 </div>
                 <div class="send-to flex flex-column">
@@ -57,7 +57,7 @@
                     <p>{{ currentInvoice[0].clientEmail }}</p>
                 </div>
             </div>
-            <div class="bottom-flex flex-column">
+            <div class="bottom flex flex-column">
                 <div class="billing-items">
                     <div class="heading flex">
                         <p>Itme Name</p>
@@ -78,7 +78,6 @@
                 </div>
             </div>
          </div>
-        {{ currentInvoice[0].invoiceId }}
     </div>
 </template>
 
@@ -122,9 +121,9 @@ import { useRoute } from 'vue-router';
         }
     }
 
-    .header
+    .header,
     .invoice-details {
-       background-color: #1e2939;
+       background-color: #1e2139;
        border-radius: 20px; 
     }
     
@@ -147,7 +146,156 @@ import { useRoute } from 'vue-router';
             
             button {
                 color: #fff;
-                
+
+            }
+        }
+    }
+
+    .invoice-details {
+        padding: 48px;
+        margin-top: 24px;
+
+        .top {
+            div {
+                color: #dfe3fa;
+                flex: 1;
+            }
+
+            .left {
+                font-size: 12px;
+                p:first-child {
+                    font-size: 24px;
+                    text-transform: uppercase;
+                    color: #fff;
+                    margin-bottom: 8px;
+                }
+
+                p:nth-child(2) {
+                    font-size: 16px;
+                }
+
+                span {
+                    color: #882eb0;
+                    margin-right: 8px;
+                }
+
+            }
+
+            .right {
+                font-size: 12px;
+                align-items: flex-end;
+            }
+        }
+
+        .middle {
+            margin-top: 50px;
+            color: #dfe3fa;
+            gap: 16px;
+
+            h4 {
+                font-size: 12px;
+                font-weight: 400;
+                margin-bottom: 12px;
+            }
+
+            p {
+                font-size: 16px;
+            }
+
+            .bill,
+            .payment {
+                flex: 1;
+            }
+
+            .payment {
+                h4:nth-child(3) {
+                    margin-top: 32px;
+                }
+
+                p {
+                    font-weight: 600;
+                }
+            }
+
+            .bill {
+                p:nth-child(2) {
+                    font-size: 16px;
+                }
+
+                p:nth-child(3) {
+                    margin-top: auto;
+                }
+
+                p {
+                    font-size: 12px;
+                }
+            }
+
+            .send-to {
+                flex: 2;
+            }
+        }
+
+        .bottom {
+            margin-top: 50px;
+            .billing-items {
+                padding: 32px;
+                border-radius: 20px 20px 0 0;
+                background-color: #252945;
+
+                .heading {
+                    font-size: 12px;
+                    color: #888eb0;
+                    margin-bottom: 32px;
+
+                    p:first-child {
+                        flex: 3;
+                        text-align: left;
+                    }
+
+                    p {
+                        flex: 1;
+                        text-align: right;
+                    }
+                }
+
+                .item {
+                    margin-bottom: 32px;
+                    font-size: 13px;
+                    color: #fff;
+
+                    &:last-child {
+                        margin-bottom: 1;
+                    }
+
+                    p:first-child {
+                        flex: 3;
+                        text-align: left;
+                    }
+
+                    p {
+                        flex: 1;
+                        text-align: right;
+                    }
+                }
+            }
+
+            .total {
+                color: #fff;
+                padding: 32px;
+                background-color: rgba(12, 14, 22, 0.7);
+                align-items: center;
+                border-radius: 0 0 20px 20px;
+
+                p {
+                    flex: 1;
+                    font-size: 12px;
+                }
+
+                p:nth-child(2) {
+                    font-size: 28px;
+                    text-align: right;
+                }
             }
         }
     }
