ef552a8148f36417e3cc2236259f739e7a54bc85
Abhishek p p <abhishekkanichery@gmail.com>
2024-11-01 08:21:24 +0530

    mark up for inovice detail view

--- pages/InvoiceView/[invoiceId].vue
+++ pages/InvoiceView/[invoiceId].vue
@@ -1,5 +1,83 @@
 <template>
-    <div class="">
+    <div v-if="currentInvoice" class="invoice-view container">
+        <nuxt-link class="nav-link flex" :to="`/`">
+            <img src="../../assets/icon-arrow-left.svg" alt="left arrow">
+            Go back
+        </nuxt-link>
+
+        <!-- Header -->
+        <div class="header flex">
+            <div class="left flex">
+                <span>Status</span>
+                <div class="status-button flex" :class="{ paid: currentInvoice[0].invoicePaid, draft: currentInvoice[0].invoiceDraft, pending: currentInvoice[0].invoicePending }">
+                <span v-if="currentInvoice[0].invoicePaid">Paid</span>
+                <span v-if="currentInvoice[0].invoiceDraft">Draft</span>
+                <span v-if="currentInvoice[0].invoicePending">Pending</span>
+            </div>
+            </div>
+            <div class="right flex">
+                <button class="dark-purple" @click="toggleEditInvoice(currentInvoice[0].docId)">Edit</button>
+                <button class="red" @click="deleteInvoice(currentInvoice[0].docId)">Delete</button>
+                <button class="green" v-if="currentInvoice[0].invoicePending" @click="updateStatusToPaid(currentInvoice[0].docId)">Mark as paid</button>
+                <button class="orange" v-if="currentInvoice[0].invoiceDraft || currentInvoice[0].invoicePaid" @click="updateStatusToPending(currentInvoice[0].docId)">Mark as pending</button>
+            </div>
+        </div>
+
+        <!-- Invoice Details -->
+         <div class="invoice-details flex flex-column">
+            <div class="top flex">
+                <div class="left flex">
+                    <p><span>#</span>{{ currentInvoice[0].invoiceId }}</p>
+                    <p>{{ currentInvoice[0].productDescription }}</p>
+                </div>
+                <div class="right flex flex-column">
+                    <p>{{ currentInvoice[0].billerStreetAddress }}</p>
+                    <p>{{ currentInvoice[0].billerCity }}</p>
+                    <p>{{ currentInvoice[0].billerZipcode }}</p>
+                    <p>{{ currentInvoice[0].billerCountry }}</p>
+                </div>
+            </div>
+            <div class="middle flex">
+                <div class="payment flex flex-column">
+                    <h4>Invoice Date</h4>
+                    <p>{{ currentInvoice[0].invoiceDate }}</p>
+                    <h4>Payment Date</h4>
+                    <p>{{ currentInvoice[0].paymentDueDate }}</p>
+                </div>
+                <div class="bill flex flex-column">
+                    <h4>Bill To</h4>
+                    <p>{{ currentInvoice[0].clientName }}</p>
+                    <p>{{ currentInvoice[0].clientStreetAddress }}</p>
+                    <p>{{ currentInvoice[0].clientCity }}</p>
+                    <p>{{ currentInvoice[0].clientZipcode }}</p>
+                    <p>{{ currentInvoice[0].clientCountry }}</p>
+                </div>
+                <div class="send-to flex flex-column">
+                    <h4>Sent To</h4>
+                    <p>{{ currentInvoice[0].clientEmail }}</p>
+                </div>
+            </div>
+            <div class="bottom-flex flex-column">
+                <div class="billing-items">
+                    <div class="heading flex">
+                        <p>Itme Name</p>
+                        <p>QTY</p>
+                        <p>Price</p>
+                        <p>Total</p>
+                    </div>
+                    <div v-for="(item, index) in currentInvoice[0].invoiceItemList" :key="index" class="item flex">
+                        <p>{{ item.itemName }}</p>
+                        <p>{{ item.qty }}</p>
+                        <p>{{ item.price }}</p>
+                        <p>{{ item.total }}</p>
+                    </div>
+                </div>
+                <div class="total flex">
+                    <p>Amount Due</p>
+                    <p>{{ currentInvoice[0].invoiceTotal }}</p>
+                </div>
+            </div>
+         </div>
         {{ currentInvoice[0].invoiceId }}
     </div>
 </template>
